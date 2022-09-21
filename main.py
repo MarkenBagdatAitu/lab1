16a585b3a93809380def609084e03215cb0eb042
MarkenBagdat <96559542+MarkenBagdatAitu@users.noreply.github.com>
2022-09-21 18:50:05 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
 import os
 
-#directory = "MArkee"
+directory = "MArkee"
 parent = "C:/doci/"
-#path = os.path.join(parent, directory)
+path = os.path.join(parent, directory)
 
 # create a directory
-#os.mkdir(path)
+os.mkdir(path)
 
-#print("'% s' created" % directory)
+print("'% s' created" % directory)
 
 # remove a directory
-#os.rmdir(path)
+os.rmdir(path)
 
 
 #update the directory
-#os.chdir(r"C:/doci/")
-#os.rename("MissMarken", "MArkee")
-#print("'% s' renamed" % directory)
+os.chdir(r"C:/doci/")
+os.rename("MissMarken", "MArkee")
+print("'% s' renamed" % directory)
 
-print(os.listdir(r"C:/doci/"))\
-
-for parent, file in os.walk(os.getcwd()):
-    print (parent)
+print(os.listdir(r"C:/doci/"))
 
