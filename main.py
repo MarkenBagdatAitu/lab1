731c766cfac81fb55b1dc9aaec3d5142f8f4ab29
MarkenBagdat <markenbagdat@gmail.com>
2022-09-21 18:44:33 +0600

    New project to push to GitHub

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+
+#directory = "MArkee"
+parent = "C:/doci/"
+#path = os.path.join(parent, directory)
+
+# create a directory
+#os.mkdir(path)
+
+#print("'% s' created" % directory)
+
+# remove a directory
+#os.rmdir(path)
+
+
+#update the directory
+#os.chdir(r"C:/doci/")
+#os.rename("MissMarken", "MArkee")
+#print("'% s' renamed" % directory)
+
+print(os.listdir(r"C:/doci/"))\
+
+for parent, file in os.walk(os.getcwd()):
+    print (parent)
+
