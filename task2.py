1e2f086c52ee2b99ecbbe77eda86f29ea8894e26
MarkenBagdat <96559542+MarkenBagdatAitu@users.noreply.github.com>
2022-09-23 01:03:29 +0600

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,12 @@
+import sys
+
+print('arguments',sys.argv)
+
+type__list = ['Python', 'JavaScript', 'Java']
+print("type of lists: ", type(type__list))
+
+type__numbers = int(1)
+print("type of numbers: ", type(type__numbers))
+
+type__string = "Hello, my name is Bagdat"
+print("type of words: ", type(type__string))
