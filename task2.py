4c94d7e39e8e53487e3f54b41c814724f3829c89
MarkenBagdat <96559542+MarkenBagdatAitu@users.noreply.github.com>
2022-09-23 23:58:45 +0600

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,11 +1,7 @@
-import sys
-
-print('arguments',sys.argv)
-
 type__list = ['Python', 'JavaScript', 'Java']
 print("type of lists: ", type(type__list))
 
-type__numbers = int(1)
+type__numbers = 758964
 print("type of numbers: ", type(type__numbers))
 
 type__string = "Hello, my name is Bagdat"
